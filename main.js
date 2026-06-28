74d69881b2d42a6d20081982fdd12312b07ffb8e
moataz1411 <mezoahmed1411@gmail.com>
2026-06-29 02:42:55 +0300

    Add loading animation for incoming messages in chat

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const typing=document.querySelector(".typing")
 const chatlist=document.querySelector(".chatlist")
+const showloading=()=>{
+    const html=`
+    <div class="message-content">
+            <img src="images/egg.webp" alt="">
+            <p class="text"></p>
+                <div class="loading">
+                    <div class="loadingbar"></div>
+                    <div class="loadingbar"></div>
+                    <div class="loadingbar"></div>
+                </div>
+            </div>
+            <span class="material-symbols-outlined">
+            content_copy
+            </span>
+    `
+    const div=document.createElement("div");
+    div.classList.add("message","incoming","loading")
+    div.innerHTML=html
+    chatlist.appendChild(div)
+}
 const handleOutGoingChat=()=>{
     userMessage=document.querySelector(".typing-input").value;
 if(!userMessage) return
@@ -14,6 +34,8 @@ div.classList.add("message","outgoing");
 div.innerHTML=html
 div.querySelector(".text").innerHTML=userMessage;
 chatlist.appendChild(div)
+typing.reset()
+setTimeout(showloading,500)
 }
 
 typing.addEventListener("submit",(e)=>{
