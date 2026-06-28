40455150cd998dab254fffbc4ad401c4a9b73c32
moataz1411 <mezoahmed1411@gmail.com>
2026-06-29 01:55:38 +0300

    Implement outgoing chat message handling in main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const typing=document.querySelector(".typing")
+const chatlist=document.querySelector(".chatlist")
 const handleOutGoingChat=()=>{
-    userMessage=document.querySelector(".typing-input").value;}
+    userMessage=document.querySelector(".typing-input").value;
+if(!userMessage) return
+const html=`
+<div class="message-content">
+            <img src="images/chill dog.png" alt="">
+            <p class="text"></p>
+            </div>
+`
+const div= document.createElement("div");
+div.classList.add("message","outgoing");
+div.innerHTML=html
+div.querySelector(".text").innerHTML=userMessage;
+chatlist.appendChild(div)
+}
 
 typing.addEventListener("submit",(e)=>{
     e.preventDefault();
