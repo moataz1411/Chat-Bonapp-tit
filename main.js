9ff10d2a125cde87f2448dcb5429dbb3d9f111bf
moataz1411 <mezoahmed1411@gmail.com>
2026-06-29 04:52:42 +0300

    feat: initialize project with Express server and Google GenAI integration
    - Added package.json with dependencies for Express, CORS, dotenv, and Google GenAI
    - Created server.js to set up an Express server
    - Implemented a POST endpoint for chat that utilizes Google GenAI to generate responses
    - Configured environment variables using dotenv

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const typing=document.querySelector(".typing")
 const chatlist=document.querySelector(".chatlist")
-const API_Key="AQ.Ab8RN6Kq3Zw-CzmAGjizHuwO7LpFeNKC7dIPm-4Xlhxvv2Mq1Q";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_Key}`;
+
 const generateAPIResponse=async(div)=>{
     const textElement=div.querySelector(".text");
     try {
-        const response=await fetch(API_URL,{method:"post",headers:{"content-Type":"application/json"},body:JSON.stringify({contents:[{role:"user",parts:[{text:userMessage}]}]})})
+        const response=await fetch("http://localhost:3000/chat",{method:"post",headers:{"content-Type":"application/json"},body:JSON.stringify({message:userMessage})}
         const data=await response.json()
-        console.log(data);
-    }catch(error){console.error(error)}
 }
 const showloading=()=>{
     const html=`
---
+++ server.js
@@ -0,0 +1,28 @@
+console.log("START");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+const ai = new GoogleGenAI({
+    apiKey:process.env.GEMINI_API_KEY,});
+app.post("/chat",async(req,res)=>{
+    try{
+        const response=await ai.models.generateContent({
+            model:"gemini-2.5-flash",contents:Message,});
+            res.json({
+                reply:response.text,});
+            }catch(error){
+                console.error(error);
+                res.status(500).json({
+                    error: "Something went wrong",
+                });
+            }
+    });
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
